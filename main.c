f6872dc662e26121e4ec5117828eef887f2e745a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-10-15 22:15:58 +0200

    Add example

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char** argv) {
+    int sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        printf("error %i", errno);
+        return -1;
+    }
+
+    int val = 1;
+    int error = setsockopt(sock, IPPROTO_IP, IP_RECVTOS, &val, sizeof(val));
+    if (error == -1) {
+        printf("error %i", errno);
+        return -1;
+    }
+
+    printf("ok\n");
+}
