e36c11436ea4877ce404a36688072fa3382caf76
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-10-15 23:07:18 +0200

    wip

--- main.c
+++ main.c
@@ -7,24 +7,16 @@
 int main(int argc, char** argv) {
     int sock = socket(AF_INET6, SOCK_DGRAM, 0);
     if (sock == -1) {
-        printf("error %i", errno);
+        printf("error %i\n", errno);
         return -1;
     }
 
     int val = 1;
     int error = setsockopt(sock, IPPROTO_IP, IP_RECVTOS, &val, sizeof(val));
     if (error == -1) {
-        printf("error %i", errno);
+        printf("error %i\n", errno);
         return -1;
     }
-
-    libc::setsockopt(
-            io.as_raw_fd(),
-            libc::IPPROTO_IPV6,
-            libc::IPV6_RECVTCLASS,
-            &on as *const _ as _,
-            std::mem::size_of_val(&on) as _,
-        )
 
     printf("ok\n");
 }
